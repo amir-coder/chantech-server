d3db2f9ef3e1fd760e6b3b61073315146949a4ae
AlmammaAmir <56568152+amir-coder@users.noreply.github.com>
2022-01-13 15:22:25 +0100

    added role

--- routers/personne.js
+++ routers/personne.js
@@ -54,6 +54,56 @@ router.get("/email/:email/mdp/:mdp", function(req, res) {
 });
 
 
+router.get("/email/:email/role/", function(req, res){
+  resdata =  {
+    "admin": 0,
+    "responsable": 0,
+    "chantierRespo": [
+      ""
+    ],
+    "ouvrier": 1,
+    "chantierAffecter": [
+      "",
+    ]
+  };
+
+  //selecting if admin or not
+  query = `select admin from personne where email= "${req.params.email}"`;
+  db.connection.query(query, function(err, data, fields) {
+    if (err) throw err;
+    resdata = (data[0]);
+  });
+
+  query = `select nomChantier from chantier
+  where Responsable in (
+  select idPersonne from personne where email = "${req.params.email}"
+  )`;
+
+  db.connection.query(query, function(err, data, fields) {
+    if (err) throw err;
+    resdata.chantierRespo = data;
+    if (data.length > 0) {
+      resdata.responsable = 0
+    }
+  });
+
+  query = `select nomChantier from chantier
+  where idchantier in (
+  select chantier from affecter where (ouvrier in (select idpersonne from personne where email = "${req.params.email}"))
+  )`;
+
+  db.connection.query(query, function(err, data, fields) {
+    if (err) throw err;
+    resdata.chantierAffecter = data;
+  });
+
+  res.json({
+    status: 200,
+    message: "Action complete successfully!"
+  });
+});
+
+
 //create new personne
 router.post("/nom/:nom/prenom/:prenom/numero/:numero/email/:email", function(req, res) {
   let query = `INSERT INTO PERSONNE(nom, prenom, numero, email) values ("${req.params.nom}", "${req.params.prenom}", ${req.params.numero}, "${req.params.email}")`;
